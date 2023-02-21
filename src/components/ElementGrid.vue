a94c188817d841b50743dbbfbf8ee707433c0c30
Don Delice Dushime <dondelicedushime@gmail.com>
2023-02-21 02:39:58 +0200

    initial commit

---
+++ src/components/ElementGrid.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="gen">
+  <div class="contEl" @click="delet(index)" v-for="(element, index) in elements" :key="(element, index)">{{element}}</div>
+</div>
+</template>
+
+<script>
+
+export default {
+  props:['elements'],
+  methods:{
+    delet(index){
+      this.$emit('deleteEl', index)
+      
+
+
+    }
+  }
+  
+}
+</script>
+<style scoped>
+.contEl{
+  background: rgb(190, 185, 185);
+  color: rgb(0, 0, 0);
+  border: 1px solid black;
+  font-family: 'Arizona';
+  min-width: 150px;
+  max-width: 200px;
+  max-height: 100px;
+  font-size: 25px;
+  text-align: center;
+  padding: 20px 0px 20px 0px;
+  margin: 20px;
+  display: flex;
+  flex-direction: row;
+  overflow-x: auto;
+}
+.contEl:hover{
+  background: rgb(237, 136, 136);
+}
+textarea{
+  display: flex;
+  flex-wrap: wrap;
+}
+.gen{
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  
+}
+
+</style>
---
+++ src/components/NewElement.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <h1>Add Elements here</h1>
+        <textarea name="" id="" required placeholder="Enter your text here..." v-model="element" cols="30" rows="10">
+
+        </textarea>
+        <button @click="newElement">Add New</button>
+        <br>
+        <span style="background:rgb(50, 150, 150); color : white; padding:3px">Click on the element in order to remove it </span>
+
+    </div>
+</template>
+
+<script>
+
+export default{
+    data(){
+        return{
+            element : ''
+        }
+    },
+    methods:{
+        newElement(){
+           
+             if(this.element!=''){
+                this.$emit('addElement', this.element);
+            this.element=""
+             }
+             else{
+                alert('The field cannot be empty')
+             }
+          
+           
+        }
+    }
+    
+}
+</script>
+<style scoped>
+div{
+    display: flex;
+    flex-direction: column;
+    align-items:center;
+    justify-content: center;
+    text-align: center;
+    margin-top: -70px;
+ 
+}
+button{
+    padding: 10px;
+    font-size: 18px;
+    border: none;
+    background: blue;
+    color: rgb(255, 255, 255);
+}
+textarea{
+    border: 2px solid blue;
+}
+</style>
