4ec897254e22f3519270e6b9d51d0056afc52320
Don Delice Dushime <107407612+dondelice257@users.noreply.github.com>
2023-02-23 01:19:37 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 </template>
 <script>
 import ElementGrid from './components/ElementGrid.vue'
-import NewElement from './components/NewElement.vue'
+import NewElement from './components/NewElement.vue'3
 
 export default {
   data(){
